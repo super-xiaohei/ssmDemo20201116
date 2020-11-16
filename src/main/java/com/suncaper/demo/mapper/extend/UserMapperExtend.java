a7538c7e64d963b5292748ee0e468c1e56c29f1a
郑义桥 <1312890342@qq.com>
2020-11-16 10:43:53 +0800

    user的Mapper接口，注意extend包下面的UserMapperExtend需要被UserMapper继承，并且每次点击重新生成代码的时候都要执行此操作，service等同理。

---
+++ src/main/java/com/suncaper/demo/mapper/extend/UserMapperExtend.java
@@ -0,0 +1,11 @@
+package com.suncaper.demo.mapper.extend;
+
+import com.suncaper.demo.entity.User;
+
+/**
+ * @author zyq
+ * @date 2020/11/12 - 18:14
+ */
+public interface UserMapperExtend {
+    User selectByUser(User user);
+}
